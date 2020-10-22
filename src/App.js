8b45be760a480a381bdb7d88fc56044a16f9ad68
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-10-22 11:17:33 -0500

    Make a controlled component out of the <textarea> with useState().

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [ text, setText ] = useState("boo!")
+
+  function handleChange(e) {
+    const {value} = e.target
+    setText(value)
+  }
+
+  console.log(text)
+
   return (
     <div>
       <header>
         <h1>Speed Typing Game!</h1>
       </header>
       <main>
-        <textarea name="" value="" onChange={() => console.log('I have been typed in!')} id="" />
+        <textarea name="" value={text} onChange={handleChange} id="" />
         <h4>Time Remaining: ###</h4>
         <button>Start</button>
         <h1>Word Count: ###</h1>
