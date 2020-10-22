4455bb48bd4b13e1ebfeb67d66803d0286a6ed94
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-10-22 13:37:05 -0500

    Make the game with useState() and useEffect().

--- src/App.js
+++ src/App.js
@@ -1,29 +1,72 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from "react"
+
+/**
+ * Challenge:
+ * 
+ * When the timer reaches 0, count the number of words the user typed in 
+ * and display it in the "Word count" section
+ * 
+ * After the game ends, make it so the user can click the Start button again
+ * to play a second time
+ */
 
 function App() {
-  const [ text, setText ] = useState("boo!")
-
-  function handleChange(e) {
-    const {value} = e.target
-    setText(value)
-  }
-
-  console.log(text)
-
-  return (
-    <div>
-      <header>
-        <h1>Speed Typing Game!</h1>
-      </header>
-      <main>
-        <textarea name="" value={text} onChange={handleChange} id="" />
-        <h4>Time Remaining: ###</h4>
-        <button>Start</button>
-        <h1>Word Count: ###</h1>
-      </main>
-    </div>
-  );
+    const STARTING_TIME = 15
+    
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        return wordsArr.filter(word => word !== "").length
+    }
+    
+    function startGame() {
+        setIsTimeRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setText("")
+    }
+    
+    function endGame() {
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+    }
+    
+    useEffect(() => {
+        if(isTimeRunning && timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        } else if(timeRemaining === 0) {
+            endGame()
+        }
+    }, [timeRemaining, isTimeRunning])
+    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button 
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
 }
 
-export default App;
- 
+export default App
